2f258064c766740c5c127620185d065d953a0c8d
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-06 19:30:13 +0200

    Establishing connection to the database

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
+
 } );
+
+ 
+app.get('/', (req, res) => {  
+   
+    res.send('Hello, World!');   
+
+} )
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://funnyvazoniaina:HxDd4nwnDVhjWFFf@crud.vfgfj2i.mongodb.net/?retryWrites=true&w=majority&appName=CRUD', {
+  
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+});
