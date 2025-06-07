dee28ae00ec9018e2625b4589bbfc67ae1c76a9f
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-07 07:18:09 +0200

    Implementing CRUD controller in server.js

--- models/product.model.js
+++ models/product.model.js
@@ -1,28 +1,29 @@
-const mongoose = require('mongoose');
-const productSchema = new mongoose.Schema({
-
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-    required: [true, "please enter product name"] 
+      type: String,
+      required: [true, "please enter product name"],
     },
-     Quantity: {
-        type: Number,
-        required: true,
-        default: 0
-     },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
     price: {
-        type: Number,
-        required: true,
-        default: 0
-     },
-    image: {
-        type: String,
-        required: false 
-     },
-         
+      type: Number,
+      required: true,
+      default: 0,
     },
-     {
-            timestamps: true
-     }
-  
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
